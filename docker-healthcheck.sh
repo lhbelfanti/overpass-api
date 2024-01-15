b76ae6ef3f4b72cdb50f87e2016c874e270feb5f
Lucas <lhbelfanti@gmail.com>
2024-01-15 15:07:21 -0300

    Organizing logs

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -2,6 +2,9 @@
 
 set -e -o pipefail
 
-OVERPASS_HEALTHCHECK=${OVERPASS_HEALTHCHECK:-'curl --noproxy "*" -qf "http://localhost/api/interpreter?data=\[out:json\];node(1);out;" | jq ".generator" |grep -q Overpass || exit 1'}
+HEALTHCHECK='curl --noproxy "*" -qf "http://localhost/api/interpreter?data=\[out:json\];node(1);out;" | jq ".generator" |grep -q Overpass || exit 1'
 
+OVERPASS_HEALTHCHECK=${OVERPASS_HEALTHCHECK:-$HEALTHCHECK}
+
+echo "Healthcheck"
 eval "${OVERPASS_HEALTHCHECK}"
